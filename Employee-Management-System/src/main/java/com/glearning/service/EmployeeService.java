36d1c9f297931de31b4f8970b4e09a057f7020eb
Kailash Nath Yadav <107490624+KailashNathYadav@users.noreply.github.com>
2023-04-04 11:53:52 +0530

    Add files via upload

---
+++ Employee-Management-System/src/main/java/com/glearning/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.glearning.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.glearning.model.Employee;
+
+public interface EmployeeService {
+	List<Employee> findAllEmployee();
+	Employee saveEmployee(Employee employee);
+	Employee findEmployeeById(long id);
+	void deleteEmployeeById(long id);
+}
